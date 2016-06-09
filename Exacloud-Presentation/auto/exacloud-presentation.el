a59273026ee5df9644eb728d1b0544a93089de25
Stephan Lindner <lindners@ohsu.edu>
2016-06-09 14:01:22 -0700

    Initial outline of presentation.

---
+++ Exacloud-Presentation/auto/exacloud-presentation.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "exacloud-presentation"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "12pt" "t" "xcolor=table")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "/home/stephan/H/Styles/style_presentation"
+    "beamer"
+    "beamer12")
+   (LaTeX-add-labels
+    "sec:orgheadline1")))
+
